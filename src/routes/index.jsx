132b0ff461d41085d2e910f81c856fbf491b6d63
JoonHyuk Lee <60730765+anshqhsh@users.noreply.github.com>
2022-05-25 21:59:34 +0900

    git conflict add file

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,27 +1,19 @@
-import Integrated from './IntegratedAdvertising'
-import styles from './Routes.module.scss'
-// import Date from '../components/datepicker/Datepicker'
-import AdCards from '../components/AdCards'
-import Navi from '../components/NavBar/Navi'
+import styles from './routes.module.scss'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import FirstPage from './page1'
+import SecondPage from './page2'
 
-const FirstPage = () => {
+const App = () => {
   return (
-    <div className={styles.firstpage}>
-      {/* <header>
-      </header>  */}
-      {/* <Date /> */}
-      <nav>
-        <Navi />
-      </nav>
-      <main className={styles.main}>
-        <div className={styles.integrated}>
-          <h1>통합 광고 현황</h1>
-          {/* <Integrated /> */}
-          <AdCards />
-        </div>
-      </main>
+    <div className={styles.app}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<FirstPage />} />
+          <Route path='dashboard' element={<SecondPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
 
-export default FirstPage
+export default App
